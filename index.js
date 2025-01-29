b5046163c5c6553fd9b295aad61ca2fdc1d282c2
hajilok <120608486+hajilok@users.noreply.github.com>
2025-01-29 17:02:02 +0700

    Update fix bug Multiaccount

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ import figlet from "figlet";
 import Web3 from "web3";
 import fs from "fs/promises";
 import JoinSpace from "./joinSpace.js";
-import { join } from "path";
-import { error } from "console";
 
 const displayBanner = () => {
     console.log(chalk.cyan(figlet.textSync('Makmum Airdrop', {
@@ -29,8 +27,8 @@ const login = async (privateKey) => {
     const getNonce = await axios.get('https://dapp-backend-large.fractionai.xyz/api3/auth/nonce');
     const nonce = getNonce.data.nonce;
     console.log(chalk.green(`Nonce: ${nonce}`));
-    // Perbarui payload dengan data dinamis
-    const issuedAt = new Date().toISOString(); // Timestamp saat ini
+
+    const issuedAt = new Date().toISOString();
     const message = `dapp.fractionai.xyz wants you to sign in with your Ethereum account:
 ${account.address}
 
@@ -42,17 +40,13 @@ Chain ID: 11155111
 Nonce: ${nonce}
 Issued At: ${issuedAt}`;
 
-    // Tanda tangani pesan
     const signature = web3.eth.accounts.sign(message, privateKey);
-
-    // Payload akhir
     const payload = {
         message,
         signature: signature.signature,
         referralCode: "66448E24"
     };
 
-    // Kirim request login
     const loginData = await axios.post('https://dapp-backend-large.fractionai.xyz/api3/auth/verify', payload, {
         headers: {
             'Content-Type': 'application/json',
@@ -62,7 +56,6 @@ Issued At: ${issuedAt}`;
     return loginData.data;
 };
 
-
 const main = async () => {
     displayBanner();
     console.log(chalk.blue('Memulai Mesin auto join space...'));
@@ -71,61 +64,57 @@ const main = async () => {
         .split('\n')
         .filter(Boolean);
 
-        while (true) {
-            for (let i = 0; i < wallet.length; i++) {
-                try {
-                    const privatekey = wallet[i];
-                    const formattedPrivateKey = privatekey.startsWith('0x') ? privatekey : '0x' + privatekey;
-                    const getlogin = await login(formattedPrivateKey);
-                    const getAiagent = await JoinSpace(getlogin.accessToken, getlogin.user.id);
-                    console.log(chalk.green(`Success login with wallet: ${getlogin.user.walletAddress} \nFractal Amount : ${getlogin.user.fractal}`));
-                    console.log(chalk.green(`Total agent: ${getAiagent.aiagentId.length}`));
-                    while (true) {
-                        for (let j = 0; j < getAiagent.aiagentId.length; j++) {
-                            const aiagentId = getAiagent.aiagentId[j];
-                            const agentName = getAiagent.nameAgent[j];
-                            try {
-                                const joinSpace = await axios.post(
-                                    `https://dapp-backend-large.fractionai.xyz/api3/matchmaking/initiate`,
-                                    { userId: getlogin.user.Id, agentId: aiagentId, entryFees: 0.001, sessionTypeId: 1 },
-                                    {
-                                        headers: {
-                                            Authorization: `Bearer ${getlogin.accessToken}`,
-                                            'Content-Type': 'application/json'
-                                        }
-                                    }
-                                );
-                            
-                                if (joinSpace.status === 200) {
-                                    console.log(chalk.green(`Success join space with ${agentName} : agentId: ${aiagentId} `));
-                                }
-                            } catch (error) {
-                                if (error.response) {
-                                    if (error.response.status === 400) {
-                                        console.log(chalk.yellow(`Failed join space with ${agentName} agent: ${aiagentId}, Reason: ${error.response.data.error}`));
-                                    } else {
-                                        console.log(chalk.yellow(`Failed join space with ${agentName} agent: ${aiagentId}, Status: ${error.response.status}, Reason: ${error.response.data.error || "Unknown"}`));
-                                        break
-                                    }
-                                } else if (error.request) {
-                                    console.log(chalk.red(`No response received. Request failed: ${error.message}`));
-                                    break
-                                } else {
-                                    console.log(chalk.red(`Error occurred: ${error.message}`));
-                                    break
-                                }
-                            }     
-                            
-                        }
+    while (true) {
+        for (let i = 0; i < wallet.length; i++) {
+            try {
+                const privateKey = wallet[i];
+                const formattedPrivateKey = privateKey.startsWith('0x') ? privateKey : '0x' + privateKey;
+                const getlogin = await login(formattedPrivateKey);
+                const getAiagent = await JoinSpace(getlogin.accessToken, getlogin.user.id);
+                console.log(chalk.green(`Success login with wallet: ${getlogin.user.walletAddress} \nFractal Amount : ${getlogin.user.fractal}`));
+                console.log(chalk.green(`Total agent: ${getAiagent.aiagentId.length}`));
+                
+                let agentCount = 0;
+                for (let j = 0; j < getAiagent.aiagentId.length; j++) {
+                    if (agentCount >= 2) {
+                        console.log(chalk.yellow('Menunggu 3 menit sebelum melanjutkan ke agen berikutnya...'));
+                        await delay(180000);
+                        agentCount = 0;
                     }
                     
-                } catch (error) {
-                    console.error(error);
+                    const aiagentId = getAiagent.aiagentId[j];
+                    const agentName = getAiagent.nameAgent[j];
+                    try {
+                        const joinSpace = await axios.post(
+                            `https://dapp-backend-large.fractionai.xyz/api3/matchmaking/initiate`,
+                            { userId: getlogin.user.id, agentId: aiagentId, entryFees: 0.001, sessionTypeId: 1 },
+                            {
+                                headers: {
+                                    Authorization: `Bearer ${getlogin.accessToken}`,
+                                    'Content-Type': 'application/json'
+                                }
+                            }
+                        );
+                    
+                        if (joinSpace.status === 200) {
+                            console.log(chalk.green(`Success join space with ${agentName} : agentId: ${aiagentId} `));
+                            agentCount++;
+                        }
+                    } catch (error) {
+                        if (error.response) {
+                            console.log(chalk.yellow(`Failed join space with ${agentName} agent: ${aiagentId}, Status: ${error.response.status}, Reason: ${error.response.data.error || "Unknown"}`));
+                        } else {
+                            console.log(chalk.red(`Error occurred: ${error.message}`));
+                        }
+                    }
                 }
+            } catch (error) {
+                console.error(error);
             }
-            console.log(chalk.blue('Menunggu 20 menit sebelum siklus berikutnya...'));
-                await delay(1200000); // Delay 10 menit
         }
+        console.log(chalk.blue('Menunggu 20 menit sebelum siklus berikutnya...'));
+        await delay(1200000);
+    }
 };
 
 main();
